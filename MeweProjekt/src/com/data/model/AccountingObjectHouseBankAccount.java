60618cfbd71f4ecbad020d87de55a14dc76ce51a
TomMeLi <thomas@familie-meder.net>
2015-01-28 16:09:50 +0100

    modified bank object

--- MeweProjekt/src/com/data/model/AccountingObjectHouseBankAccount.java
+++ MeweProjekt/src/com/data/model/AccountingObjectHouseBankAccount.java
@@ -7,27 +7,15 @@ import javax.persistence.Entity;
 @DiscriminatorValue("HouseBankAccount")
 public class AccountingObjectHouseBankAccount extends AccountingObjectCategory {
 
-//	private OnlineBankingParameters onlineParams;
-//	//@OneToOne
-//	//@Column(name="BankReference")
-//	private BankReference bankReference;
-//	
-//	
-//	public OnlineBankingParameters getOnlineBankingParams(){
-//		return onlineParams;
-//	}
-//
-//	public void setOnlineBankingParameters(OnlineBankingParameters params){
-//		this.onlineParams = params;
-//	}
-//
-//	public BankReference getBankReference() {
-//		return bankReference;
-//	}
-//
-//	public void setBankReference(BankReference bankReference) {
-//		this.bankReference = bankReference;
-//	}
+	private BankReference bankReference;
+
+	public BankReference getBankReference() {
+		return bankReference;
+	}
+
+	public void setBankReference(BankReference bankReference) {
+		this.bankReference = bankReference;
+	}
 
 	
 }
